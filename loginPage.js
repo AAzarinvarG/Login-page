2f4b390e09ab87077ff20d97f322fb47699c23b6
abolfazl <abolfazl@gmail.com>
2023-04-01 04:02:55 +0430

    Steps to make time and background photos

--- loginPage.js
+++ loginPage.js
@@ -2,6 +2,9 @@ const $ = document;
 
 let inputPass = $.querySelector('#input-password');
 let iconEye = $.querySelector('#icon-eye');
+let divRight = $.querySelector('#div-right');
+let body = $.querySelector('body');
+let login = $.querySelector('#login');
 let boolean = false;
 
 iconEye.addEventListener('click', () => {
@@ -19,3 +22,48 @@ iconEye.addEventListener('click', () => {
 
     }
 });
+
+setInterval(() => {
+    let nowHour = new Date().getHours();
+
+    if (window.innerWidth > 992) {
+        body.style.backgroundImage = '';
+
+        if (nowHour >= 6) {
+            divRight.style.backgroundImage = 'url(img/morning.jpg)';
+
+        } else if (nowHour >= 9) {
+            divRight.style.backgroundImage = 'url(img/sunny.jpg)';
+
+        } else if (nowHour >= 16) {
+            divRight.style.backgroundImage = 'url(img/afternoon.jpg)';
+
+        } else if (nowHour >= 20 || nowHour >= 1) {
+            divRight.style.backgroundImage = 'url(img/night.jpg)';
+
+        }
+
+    } else {
+
+        if (nowHour >= 6) {
+            body.style.backgroundImage = 'url(img/morning.jpg)';
+
+        } else if (nowHour >= 9) {
+            body.style.backgroundImage = 'url(img/sunny.jpg)';
+
+        } else if (nowHour >= 16) {
+            body.style.backgroundImage = 'url(img/afternoon.jpg)';
+
+        } else if (nowHour >= 20 || nowHour >= 1) {
+            body.style.backgroundImage = 'url(img/night.jpg)';
+
+        }
+
+    }
+
+    if (body.style.backgroundImage == 'url("img/night.jpg")') {
+        login.style.border = '2px solid white';
+    }
+
+}, 1000);
+
